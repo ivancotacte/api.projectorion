3505bd769e1228bf47db538b79f5c43a31cd1f81
ivancotacte <cotactearmenion@gmail.com>
2023-12-16 15:30:06 +0800

    first commit

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
